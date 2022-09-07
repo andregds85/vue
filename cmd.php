f29e8bb3ac9b17b04fd0297e3cc533de78bd1aec
André <andregds85@gmail.com>
2022-09-07 18:13:54 -0300

    vue1

---
+++ cmd.php
@@ -0,0 +1,16 @@
+video base 
+https://www.youtube.com/watch?v=p-xkYfY2Xo0&t=373s
+
+https://cli.vuejs.org/
+
+Instalando vue Cli 
+npm install vue@next
+npm install -g @vue/cli
+
+Verificando a versão do vue 
+vue --version 
+
+Criar um projeto com Vue 
+vue create vuecad
+
+parei em 11:00
---
+++ cmdGiT.blade.php
@@ -0,0 +1,14 @@
+Comandos de Identificação 
+
+git config --global user.name "André"
+git config --global user.email andregds85@gmail.com
+
+Adiciona todas as alterações 
+git add --all
+
+Cria um commit com uma mensagem 
+git commit -vue1"
+
+git push -u origin master
+git push --force origin main
+---------------------------------------------------------------------
